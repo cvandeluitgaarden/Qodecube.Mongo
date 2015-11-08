a28185fea19b13a1bb23be991c26f72e6b3cfe26
Christian van de Luitgaarden <c.vandeluitgaarden@outlook.com>
2015-11-08 11:20:50 +0100

    Initial commit

---
+++ src/Qodecube.Mongo/Attributes/CollectionNameAttribute.cs
@@ -0,0 +1,15 @@
+﻿namespace Qodecube.Mongo.Attributes
+{
+    using System;
+
+    [AttributeUsage(AttributeTargets.Property)]
+    public class CollectionNameAttribute: Attribute
+    {
+        public string Name { get; set; }
+
+        public CollectionNameAttribute(string name)
+        {
+            this.Name = name;
+        }
+    }
+}
---
+++ src/Qodecube.Mongo/MongoDbContext.cs
@@ -0,0 +1,51 @@
+﻿namespace Qodecube.Mongo
+{
+    using MongoDB.Driver;
+    using Attributes;
+    using System;
+    using System.Configuration;
+    using System.Linq;
+    using System.Reflection;
+
+    public abstract class MongoDbContext
+    {
+        public MongoDbContext(string nameOrConnectionstring)
+        {
+            var mongoUrl = new MongoUrl(GetConnenctionUrl(nameOrConnectionstring));
+            InstantiateMongoCollections(new MongoClient(mongoUrl).GetDatabase(mongoUrl.DatabaseName));
+        }
+
+        private void InstantiateMongoCollections(IMongoDatabase database)
+        {
+            MethodInfo getCollectionMethod = database.GetType().GetMethod("GetCollection");
+            foreach(var collectionType in this.GetType().GetProperties()
+                .Where(prop => prop.PropertyType.IsGenericType && prop.PropertyType.GetGenericTypeDefinition() == typeof(IMongoCollection<>)))
+            {
+                var arg = collectionType.PropertyType.GetGenericArguments()[0];
+                var value = getCollectionMethod.MakeGenericMethod(arg)?.Invoke(database, new object[] { GetCollectionName(collectionType), null });
+                collectionType.SetValue(this, value);
+            }
+        }
+
+        private string GetCollectionName(PropertyInfo collectionType)
+        {
+            var attribute = collectionType.GetCustomAttribute<CollectionNameAttribute>();
+            if(attribute != null)
+            {
+                return attribute.Name;
+            }
+
+            return collectionType.Name;
+        }
+
+        private string GetConnenctionUrl(string nameOrConnectionstring)
+        {
+            if (!nameOrConnectionstring.StartsWith("mongodb://", StringComparison.OrdinalIgnoreCase))
+            {
+                nameOrConnectionstring = ConfigurationManager.ConnectionStrings[nameOrConnectionstring].ConnectionString;
+            }
+
+            return nameOrConnectionstring;
+        }
+    }
+}
